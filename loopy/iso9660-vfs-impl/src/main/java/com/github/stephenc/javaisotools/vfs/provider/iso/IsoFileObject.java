8b7a722f43afe0f33b37982f6ff0c13e82120351
Stephen Connolly <stephen.alan.connolly@gmail.com>
2010-10-05 12:17:54 +0100

    refactoring

--- loopy/iso9660-vfs-impl/src/main/java/com/github/stephenc/javaisotools/vfs/provider/iso/IsoFileObject.java
+++ loopy/iso9660-vfs-impl/src/main/java/com/github/stephenc/javaisotools/vfs/provider/iso/IsoFileObject.java
@@ -16,7 +16,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-package com.github.stephenc.javaisotools.loopy.vfs.provider.iso;
+package com.github.stephenc.javaisotools.vfs.provider.iso;
 
 import java.io.InputStream;
 import java.util.HashSet;
