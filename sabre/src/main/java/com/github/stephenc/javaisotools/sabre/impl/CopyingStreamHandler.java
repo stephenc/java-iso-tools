4ffdba48d9d2891390a1fac16ccc2fe4eb8cda76
Stephen Connolly <stephen.alan.connolly@gmail.com>
2010-10-05 23:25:20 +0100

    use a standardised (c) in the copyright headers

--- sabre/src/main/java/com/github/stephenc/javaisotools/sabre/impl/CopyingStreamHandler.java
+++ sabre/src/main/java/com/github/stephenc/javaisotools/sabre/impl/CopyingStreamHandler.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2010. Stephen Connolly
- * Copyright (C) 2006. Michael Hartle <mhartle@rbg.informatik.tu-darmstadt.de>.
+ * Copyright (c) 2006. Michael Hartle <mhartle@rbg.informatik.tu-darmstadt.de>.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
