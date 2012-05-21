e98163f2c4fa574bba829ab87e56d5602ebca4be
William Whittle <william.whittle@gmail.com>
2012-05-21 22:21:54 +0100

    Working
    Now creates an ISO image as an artefact that can be deployed to the
    repository. Still has lots of diagnostic output that needs removing.

--- iso9660-maven-plugin/src/main/java/com/github/stephenc/javaisotools/maven/Iso9660Archiver.java
+++ iso9660-maven-plugin/src/main/java/com/github/stephenc/javaisotools/maven/Iso9660Archiver.java
@@ -3,11 +3,15 @@ package com.github.stephenc.javaisotools.maven;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.codehaus.plexus.archiver.AbstractArchiver;
 import org.codehaus.plexus.archiver.ArchiveEntry;
+import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.ResourceIterator;
+import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.util.StringUtils;
 
 import com.github.stephenc.javaisotools.iso9660.ConfigException;
@@ -19,10 +23,12 @@ import com.github.stephenc.javaisotools.iso9660.impl.CreateISO;
 import com.github.stephenc.javaisotools.iso9660.impl.ISO9660Config;
 import com.github.stephenc.javaisotools.iso9660.impl.ISOImageFileHandler;
 import com.github.stephenc.javaisotools.joliet.impl.JolietConfig;
+import com.github.stephenc.javaisotools.maven.Iso9660ArchiverException.Type;
 import com.github.stephenc.javaisotools.rockridge.impl.RockRidgeConfig;
 import com.github.stephenc.javaisotools.sabre.HandlerException;
 import com.github.stephenc.javaisotools.sabre.StreamHandler;
 
+@Component(role = Archiver.class, hint = "iso9660")
 public class Iso9660Archiver extends AbstractArchiver {
 	private String systemId;
 	private String volumeId;
@@ -115,25 +121,96 @@ public class Iso9660Archiver extends AbstractArchiver {
 		ISO9660RootDirectory root = new ISO9660RootDirectory();
 
 		try {
+			Map<String, ISO9660Directory> directories = new TreeMap<String, ISO9660Directory>() {
+				@Override
+				public ISO9660Directory put(String path, ISO9660Directory dir) {
+					System.out.println("directories.put('" + path + "', '"
+							+ dir.getName() + "')");
+					return super.put(path, dir);
+				}
+			};
+			Map<String, ISO9660File> files = new TreeMap<String, ISO9660File>() {
+				@Override
+				public ISO9660File put(String path, ISO9660File file) {
+					System.out.println("files.put('" + path + "', '"
+							+ file.getName() + "')");
+					return super.put(path, file);
+				}
+			};
+
+			for (ResourceIterator i = getResources(); i.hasNext();) {
+				ArchiveEntry entry = i.next();
+
+				System.out.println("Resource: "
+						+ entry.getResource().getName()
+						+ "; type = "
+						+ (ArchiveEntry.FILE == entry.getType() ? "File"
+								: "Directory"));
+			}
+
+			/*
+			 * Prepare a map of all paths to ISO objects
+			 */
+			directories.put("", root);
+
 			for (ResourceIterator i = getResources(); i.hasNext();) {
 				ArchiveEntry entry = i.next();
 
 				switch (entry.getType()) {
-				case ArchiveEntry.FILE:
-					root.addFile(new ISO9660File(new ArchiveEntryDataReference(
-							entry), entry.getResource().getName(), entry
-							.getResource().getLastModified()));
+				case ArchiveEntry.DIRECTORY: {
+					ISO9660Directory dir;
+
+					dir = new ISO9660Directory(getName(entry));
+
+					directories.put(getPath(entry), dir);
+
+					String path = getPath(entry);
+					while ((path = getParentPath(path)) != "") {
+						dir = new ISO9660Directory(getName(path));
+						directories.put(path, dir);
+					}
+
 					break;
-				case ArchiveEntry.DIRECTORY:
-					root.addDirectory(new ISO9660Directory(entry.getResource()
-							.getName()));
+				}
+				case ArchiveEntry.FILE: {
+					ISO9660File file;
+					ISO9660Directory dir;
+
+					/* Create the file */
+					file = new ISO9660File(
+							new ArchiveEntryDataReference(entry),
+							getName(entry), entry.getResource()
+									.getLastModified());
+
+					files.put(getPath(entry), file);
+
+					String path = getPath(entry);
+					while ((path = getParentPath(path)) != "") {
+						dir = new ISO9660Directory(getName(path));
+						directories.put(path, dir);
+					}
+
 					break;
+				}
 				default:
 					throw new Iso9660ArchiverException(
-							Iso9660ArchiverException.Type.UnsupportedEntryType,
-							"Unsupported type of archive entry for resource "
-									+ entry.getName());
+							Type.UnsupportedEntryType, "Unknown entry type");
 				}
+			}
+
+			/* Put ISO objects as children of their parents */
+			for (Map.Entry<String, ISO9660Directory> e : directories.entrySet()) {
+				if (!e.getKey().equals("")) {
+					ISO9660Directory parent = directories.get(getParentPath(e
+							.getKey()));
+					parent.addDirectory(e.getValue());
+				}
+			}
+
+			for (Map.Entry<String, ISO9660File> e : files.entrySet()) {
+				ISO9660Directory parent = directories.get(getParentPath(e
+						.getKey()));
+				parent.addFile(e.getValue());
 			}
 
 			StreamHandler streamHandler = new ISOImageFileHandler(new File(
@@ -162,6 +239,32 @@ public class Iso9660Archiver extends AbstractArchiver {
 		} catch (ConfigException e) {
 			throw new ArchiverException(e.getMessage(), e);
 		}
+	}
+
+	private String getParentPath(ArchiveEntry entry) {
+		return getParentPath(entry.getName());
+	}
+
+	private String getParentPath(String path) {
+		String parent;
+
+		parent = path.contains("/") ? path.substring(0, path.lastIndexOf('/'))
+				: "";
+
+		return parent;
+	}
+
+	private String getPath(ArchiveEntry entry) {
+		return entry.getName();
+	}
+
+	private String getName(String path) {
+		return path.contains("/") ? path.substring(path.lastIndexOf('/') + 1)
+				: path;
+	}
+
+	private String getName(ArchiveEntry entry) {
+		return getName(entry.getName());
 	}
 
 	private void applyConfig(StandardConfig config) throws ConfigException {
